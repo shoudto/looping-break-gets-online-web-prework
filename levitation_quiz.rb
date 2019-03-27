53c9a6bc166383986e5cf8b9901aef0159b6e020
Oscar Shoudt <shoudto@gmail.com>
2019-03-27 21:05:29 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,6 @@
 
 def levitation_quiz
-  if gets == Wingardium Leviosa
+  if answer == Wingardium Leviosa
   puts "What is the spell that enacts levitation?"
   answer = gets 
   puts "You passed the quiz! "
