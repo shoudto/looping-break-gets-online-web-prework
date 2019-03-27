2be0deba8f525b023ab7ab85a63625316677d478
Oscar Shoudt <shoudto@gmail.com>
2019-03-27 20:41:44 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,9 +1,9 @@
 
 def levitation_quiz
-  while 
-	puts "What is the spell that enacts levitation?"
-  Wingardium Leviosa = gets
-end
+  while
+    puts "What is the spell that enacts levitation?"
+    Wingardium Leviosa = gets
+  end
 end
 
 
